a0ba9066429472fe8e03f37442add4f882e532a7
Nicholas Tocco <ntocco@ibm.com>
2024-10-31 23:21:14 -0400

    Code cleanup

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -12,32 +12,43 @@ function App() {
   const isSidePanelOpen = useSelector(state => state.sidePanel.open)
   const selectedTab = useSelector(state => state.selectedTab.tabName)
 
-  const displayContent = () =>{
-    if(selectedTab === 'explore'){
-      return(
-        <MapProvider>
-          <DrillDownMap/>
-          {isSidePanelOpen && <SidePanel/>}
-        </MapProvider>
-      )
-    } else if(selectedTab === 'home'){
-      return(
-        <div>
-          <h1>home page</h1>
-        </div>
-      )
-    } else if(selectedTab === 'contact'){
-      return(
-        <div>
-          <h1>contact page</h1>
-        </div>
-      )
-    } else if(selectedTab === 'pricing'){
-      return(
-        <div>
-          <h1>pricing page</h1>
-        </div>
-      )
+  const displayContent = () => {
+    switch (selectedTab) {
+      case 'explore':
+        return (
+          <MapProvider>
+            <DrillDownMap />
+            {isSidePanelOpen && <SidePanel />}
+          </MapProvider>
+        );
+  
+      case 'home':
+        return (
+          <div>
+            <h1>home page</h1>
+          </div>
+        );
+  
+      case 'contact':
+        return (
+          <div>
+            <h1>contact page</h1>
+          </div>
+        );
+  
+      case 'pricing':
+        return (
+          <div>
+            <h1>pricing page</h1>
+          </div>
+        );
+  
+      default:
+        return (
+          <div>
+            <h1>no page matched</h1>
+          </div>
+        ); 
     }
   }
 
